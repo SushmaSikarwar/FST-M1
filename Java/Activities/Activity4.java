44d2640334eb6f1a7374c8e1d6ae6042b182908d
Sushma Sikarwar <>
2022-05-06 14:11:15 +0530

    Added Activity4

---
+++ Java/Activities/Activity4.java
@@ -0,0 +1,45 @@
+package activities;
+
+import java.util.Arrays;
+
+public class Activity4 {
+	
+	
+	static void insertionSort(int array[]) {
+		
+		int size = array.length;
+		
+			for (int i = 1; i < size; i++) {
+			
+				int key = array[i];
+				int j = i-1;
+				
+				while (j >= 0 && key < array[j]) {
+					
+					array [j+1] = array [j];
+					
+					--j;
+				}
+			
+				array[j + 1] = key;		
+			
+		}
+	}	
+	
+	public static void main(String[] args) {
+		
+		int array[] = { 18, 11, 12, 19, 15};
+		
+		System.out.println("Array before Sorting:");
+		
+		System.out.println(Arrays.toString(array));
+		
+		insertionSort(array);
+		
+		System.out.println("Array after Sorting:");
+		
+		System.out.println(Arrays.toString(array));
+				
+	}
+
+}
