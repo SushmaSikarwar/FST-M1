04577e1f98c4def79be231e6bbc0ecdad189f511
Sushma Sikarwar <>
2022-05-25 17:22:18 +0530

    SQL Activity 2 & 3 Done

---
+++ SQL/Activities/Scripts/Activity_2.sql
@@ -0,0 +1,8 @@
+INSERT INTO salesman (salesman_id,salesman_name, salesman_city,comission 
+ )  
+	VALUES  
+	    (5005,'Pit Alex','London',11), 
+        (5006,'McLyon','Paris',14), 
+        (5007,'Paul Adam','Rome',13), 
+        (5003,'Lauson Hen','San Jose',12),
+		(5008,'James Houston','London',18);
