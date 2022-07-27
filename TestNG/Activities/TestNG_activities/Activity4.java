0dc40baae4b062377ccc8475e432418051567f05
Sushma Sikarwar <>
2022-07-27 18:46:04 +0530

    TestNG Activities Completed

---
+++ TestNG/Activities/TestNG_activities/Activity4.java
@@ -0,0 +1,11 @@
+package TestNG_activities;
+
+import org.testng.annotations.Test;
+
+public class Activity4 {
+
+    @Test
+    public void TestCase() {
+        System.out.println("Activity 4 is comprised of Demo1 & Demo2");
+    }
+}
---
+++ TestNG/Activities/TestNG_activities/Demo1.java
@@ -0,0 +1,16 @@
+package TestNG_activities;
+
+import org.testng.annotations.Test;
+
+public class Demo1 {
+
+    @Test
+    public void firstTestCase() {
+        System.out.println("I'm in first test case from demoOne Class");
+    }
+
+    @Test
+    public void secondTestCase() {
+        System.out.println("I'm in second test case from demoOne Class");
+    }
+}
